02a2c08b53c2ea6a6e9c836d3f7974d17683b94f
F0Xm1 <andriy.parilo@gmail.com>
2025-12-19 13:47:54 +0200

    fix wrong file name

--- image-deploy.sh
+++ image-deploy.sh
@@ -3,11 +3,11 @@ set -x
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-ssh-keyscan -H 35.225.117.203 >> ~/.ssh/known_hosts
+ssh-keyscan -H 35.225.119.213 >> ~/.ssh/known_hosts
 
-ssh ubuntu@35.225.117.203 "sudo users" || true
+ssh ubuntu@35.225.119.213 "sudo users" || true
 
-export DOCKER_HOST=ssh://ubuntu@35.225.117.203
+export DOCKER_HOST=ssh://ubuntu@35.225.119.213
 
 docker compose build backend
 docker compose up -d backend
